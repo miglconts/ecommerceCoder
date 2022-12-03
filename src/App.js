c1ec8f41c22908dcc8355ba38087655d43e5814f
Miguel Angel Contreras <miguelangelcontrerascruz@gmail.com>
2022-12-03 11:17:14 -0600

    que

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
-
 import ItemListContainer from "./componets/itemsListContainer/ItemListContainer";
 import Navbar from "./componets/navBar/Navbar";
 
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 function App() {
   return (
-    <div className="App">
-      <header className=" ">
+    <BrowserRouter>
         <Navbar></Navbar>
-      </header>
-      <ItemListContainer greeting="Mundo"></ItemListContainer>
+    <Routes>
+      <Route path="/"  element={<ItemListContainer greeting="Mundo"></ItemListContainer>}/>
+    </Routes>
+    <div className="App">
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/componets/navBar/Navbar.js
+++ src/componets/navBar/Navbar.js
@@ -5,35 +5,35 @@ export default function Navbar() {
   return (
     <nav className=" flex items-center justify-between flex-wrap bg-blue-300 p-6">
       <div className="container flex  w-6/12 items-start flex-wrap mx-auto cursor-pointer">
-        <div class="flex items-center flex-shrink-0 text-sky-900 mr-16 ">
-          <p class="font-black font-mono text-4xl tracking-wide hover:font-thin">
+        <div className="flex items-center flex-shrink-0 text-sky-900 mr-16 ">
+          <p className="font-black font-mono text-4xl tracking-wide hover:font-thin">
             KILLETTER
           </p>
         </div>
 
-        <div class="w-full block flex-grow text-left lg:flex lg:items-center lg:w-auto">
-          <div class="text-xl lg:flex-grow">
+        <div className="w-full block flex-grow text-left lg:flex lg:items-center lg:w-auto">
+          <div className="text-xl lg:flex-grow">
             <a
               href="/"
-              class="block px-5 mx-1 border-b-4 border-blue-300 hover:border-blue-600   mt-4 lg:inline-block lg:mt-0 text-blue-900 hover:text-blue-600 mr-4"
+              className="block px-5 mx-1 border-b-4 border-blue-300 hover:border-blue-600   mt-4 lg:inline-block lg:mt-0 text-blue-900 hover:text-blue-600 mr-4"
             >
               Blog
             </a>
             <a
               href="/"
-              class="block px-5 mx-1 border-b-4 border-blue-300 hover:border-blue-600   mt-4 lg:inline-block lg:mt-0 text-blue-900 hover:text-blue-600 mr-4"
+              className="block px-5 mx-1 border-b-4 border-blue-300 hover:border-blue-600   mt-4 lg:inline-block lg:mt-0 text-blue-900 hover:text-blue-600 mr-4"
             >
               Store
             </a>
             <a
               href="/"
-              class="block px-5 mx-1 border-b-4 border-blue-300 hover:border-blue-600   mt-4 lg:inline-block lg:mt-0 text-blue-900 hover:text-blue-600"
+              className="block px-5 mx-1 border-b-4 border-blue-300 hover:border-blue-600   mt-4 lg:inline-block lg:mt-0 text-blue-900 hover:text-blue-600"
             >
               About
             </a>
           </div>
           <div>
-            <a href="/" class="">
+            <a href="/" className="">
               <CartWidget></CartWidget>
             </a>
           </div>
